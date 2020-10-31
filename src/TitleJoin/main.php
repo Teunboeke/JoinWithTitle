e48ee1699c5d107c61c3b218d180167f744d5f45
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2020-10-31 20:00:43 +0100

    Update main.php

--- src/TitleJoin/main.php
+++ src/TitleJoin/main.php
@@ -1,1 +1,9 @@
+<?php
 
+namespace TitleJoin;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\Listener;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
