ba56bca51ae2cfb7d3481bf067fdb9ace3a59a0f
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2020-10-31 20:21:35 +0100

    Create SendTitle.php

---
+++ src/Teunboeke/TitleJoin/SendTitle.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Titlejoin;
+
+use pocketmine\plugin\Plugin;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\Player;
+use pocketmine\Server;
+
+class SendTitle extends PluginTask{
+
+    private $plugin;
+        private $player;
+        
+            public function __construct(Main $plugin, Player $player){
+            		    parent::__construct($plugin, $player);
+                    		    $this->plugin = $plugin;
+                            $this->player = $player;
+                            	  }
+                                
+                                    public function onRun(int $currentTick){
+                                            $title = $this->plugin->getConfig()->get("Title");
+                                                    $subtitle = $this->plugin->getConfig()->get("Subtitle");
+                                                            $fadein = $this->plugin->getConfig()->get("Fade-In");
+                               $lenght = $this->plugin->getConfig()->get("Lenght");                	  
+                               $fadeout = $this->plugin->getConfig()->get("Fade-Out");
+                                       $this->player->addTitle($this->plugin->translateColors($title, $subtitle, $fadein, $lenght, $fadeout));
+                                           }
+                                        }
